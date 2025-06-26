ab54f09cf7a309385efeacd2c28d97dbd144a0c1
AntonioGoncalvesSousa <antonio.sousa@sptech.school>
2025-06-26 00:19:27 -0300

    Alterando tamanho da navbar

--- sunffly-crew/src/components/navBar/navBar.jsx
+++ sunffly-crew/src/components/navBar/navBar.jsx
@@ -5,7 +5,7 @@ import Sunny from "../../assets/images/sunny.png"
 const navBar = () => {
     return (
         <>
-            <nav className="bg-black h-24 w-screen fixed bottom-0 left-0 z-50 flex flex-row justify-around items-center ">
+            <nav className="bg-black h-20 w-screen fixed bottom-0 left-0 z-50 flex flex-row justify-around items-center ">
                 <House className="text-white h-8 w-8"/>
                 <AlignJustify className="text-white h-8 w-8"/>
                 <img src={Sunny} className="h-9 w-10"/>
